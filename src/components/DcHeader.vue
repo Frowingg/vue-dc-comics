5ffad554214542269b868a55c21545bf63897383
Frowingg <edbico0@gmail.com>
2022-07-06 16:20:37 +0200

    header (da finire)

--- src/components/DcHeader.vue
+++ src/components/DcHeader.vue
@@ -12,6 +12,9 @@
 
                 <div id="header-nav">
                     <ul>
+                        <!-- <li v-for="(link, index) in menuLink" :key="index" :class="{ 'current': link.current}">
+                            <a :href="link.url"> {{ link.text }} </a>
+                        </li> -->
                         <li> <a href="">CHARACTERS</a></li>
                         <li> <a href="">COMICS</a></li>
                         <li> <a href="">MOVIES</a></li>
@@ -33,6 +36,65 @@
 </template>
 
 <script>
+    export default {
+        name: 'DcHeader',
+        data() {
+            return {
+                menuLinks: [
+                    {
+                        text: 'CHARACTERS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'COMICS',
+                        url: '#',
+                        current: true,
+                    },
+                    {
+                        text: 'MOVIES',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'TV',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'GAMES',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'COLLECTIBLES',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'VIDEOS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'FANS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'NEWS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'SHOP',
+                        url: '#',
+                        current: false,
+                    },
+                ]
+            }
+        }
+    }
 
 </script>
 
