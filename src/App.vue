dfa30fadee5be08a51902232b3753fbfbc7b3aee
Frowingg <edbico0@gmail.com>
2022-07-07 15:13:03 +0200

    work in progress

--- src/App.vue
+++ src/App.vue
@@ -17,18 +17,16 @@
 </template>
 
 <script>
-import DcHeader from './components/DcHeader.vue'
-import DcMain from './components/DcMain.vue'
-import DcFooter from './components/DcFooter.vue'
-
-
+import DcHeader from './components/DcHeader.vue';
+import DcMain from './components/DcMain.vue';
+import DcFooter from './components/DcFooter.vue';
 
 export default {
   name: 'App',
   components: {
     DcHeader,
     DcMain,
-    DcFooter
+    DcFooter,
   }
 }
 </script>
---
+++ src/components/DcComicCard.vue
@@ -0,0 +1,49 @@
+<template>
+
+        <div class="card">
+            <div class="card-img">
+                <img :src="infoDet.thumb" alt="infoDet.series">
+            </div>
+            <p>{{ infoDet.series }}</p>
+        </div>
+        
+
+
+</template>
+
+<script>
+export default {
+    name: 'DcComicCard',
+    props: {
+        infoDet: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+section{
+    // display: flex;
+    // flex-wrap: wrap;
+}
+.card{
+    width: calc(100% / 6 - 20px);
+    max-height: 200px;
+    margin-right: 20px;
+    color: white;
+    padding-bottom: 8px;
+
+    .card-img{
+        width: 100%;
+        height: 70%;
+    }
+
+    img{
+        width: 100%;
+        height: 100%;
+    }
+    p{
+        margin-top: 6px
+    }
+}
+
+</style>
